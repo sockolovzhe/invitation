4610b8039ca303d6127ae8406ab0f62dd115e1c6
git2 <sockolovzhe@gmail.com>
2025-05-24 12:37:53 +0500

    фикс динамического роутинга

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 // @ts-ignore
 import Title from '../components/Title.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/:userId',
